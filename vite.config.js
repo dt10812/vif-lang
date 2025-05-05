4a5eaa14bbafaf3fe08fc377b2f49ca064b0aaff
dt10812 <ducthinh100812@gmail.com>
2025-05-05 19:36:42 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
-import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
-import { cartographer } from "@replit/vite-plugin-cartographer";
-import { fileURLToPath } from "url";
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import path from "path"
+import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal"
+import { cartographer } from "@replit/vite-plugin-cartographer"
+import { fileURLToPath } from "url"
 
-const __dirname = fileURLToPath(new URL(".", import.meta.url));
+const __dirname = fileURLToPath(new URL(".", import.meta.url))
 
 export default defineConfig({
   plugins: [
@@ -14,22 +14,22 @@ export default defineConfig({
     ...(process.env.NODE_ENV !== "production" &&
     process.env.REPL_ID !== undefined
       ? [cartographer()]
-      : []),
+      : [])
   ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "client", "src"),
       "@shared": path.resolve(__dirname, "shared"),
-      "@assets": path.resolve(__dirname, "attached_assets"),
-    },
+      "@assets": path.resolve(__dirname, "attached_assets")
+    }
   },
   root: path.resolve(__dirname, "client"),
   build: {
     outDir: path.resolve(__dirname, "dist/public"),
-    emptyOutDir: true,
+    emptyOutDir: true
   },
   server: {
     port: 3000, // Local server port
-    open: true, // Auto opens browser
-  },
-});
+    open: true // Auto opens browser
+  }
+})

f692b991fb0e277da9434540649c1b64fc6f47e9
dt10812 <ducthinh100812@gmail.com>
2025-05-05 19:30:16 +0700

    Update and rename vite.config.ts to vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,29 @@
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import path from "path"
+import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal"
+import cartographer from "@replit/vite-plugin-cartographer" // Import synchronously
+
+export default defineConfig({
+  plugins: [
+    react(),
+    runtimeErrorOverlay(),
+    ...(process.env.NODE_ENV !== "production" &&
+    process.env.REPL_ID !== undefined
+      ? [cartographer()]
+      : [])
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "client", "src"), // `import.meta.dirname` replaced with `__dirname` for compatibility
+      "@shared": path.resolve(__dirname, "shared"),
+      "@assets": path.resolve(__dirname, "attached_assets")
+    }
+  },
+  root: path.resolve(__dirname, "client"),
+  build: {
+    outDir: path.resolve(__dirname, "dist/public"),
+    emptyOutDir: true
+  }
+})
+
